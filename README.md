6195bd1534e48f0c4a4f3d7d84c29d7893a5fd78
Ames <ames@Lilys-MacBook-Air-2.local>
2018-02-19 09:14:42 -0500

    cleanup, edits to readme

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # To Do App
 
+
 ### Steps to Run the application:
 
-<- install all the required modules specified in package.json as ->
-1. npm install 
+- Install all the required modules specified in package.json as:
+### `npm install`
 
-<- Run the app ->
-2. npm start
 
-<- Build with ->
-2. npm run build
+- Run:
+### `npm start` 
 
+
+- Run build:
+### `npm run build`
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,30 @@
 import '../css/global.scss';
 
-(function(){
+
+
+var Todo = (function (){
 	
 	"use strict";
 
+	
 	/**
+    * Global Variables
+    */
+    var taskList = document.getElementById('task-list');
+    var todayLong = new Date();
+
+    /**
     * Objects 
     */
 
-    var taskList = document.getElementById('task-list');
-
 	var taskObject = {
 
-		allTasks: JSON.parse(localStorage.getItem('data')) || [],
+		// Get initial data if any
+		allTasks: JSON.parse(localStorage.getItem('data')) || [], 
 
 		createTask: function(id, name, description, dueDate, completed, dueNow, overdue) {
-			this.allTasks.push({
+
+			this.allTasks.push({ 
 				id: id,
 				name: name,
 				description: description,
@@ -24,8 +33,7 @@ import '../css/global.scss';
 				dueNow: false,
 				overdue: false
 			});
-			// console.log(this.allTasks);
-		}, //createTask
+		}, 
 		getItemById: function (id, allTasks) {
 			for (var i = 0; i < allTasks.length; i++) {
 				if (allTasks[i].id == id) {
@@ -51,27 +59,19 @@ import '../css/global.scss';
 				var toggled = task.classList.toggle('toggleCompleted');
 				var id = task.getAttribute('id');
 				let thisCompleted = this.allTasks[id].completed;
-
+				var count;
 				if(thisCompleted === false){
 					thisCompleted = true;
-					completedCheck.checked = true;
+					count++;
 				}else{
 					thisCompleted = false;
 				}
-				//keep the checkbox on refresh!!
-				// if(myData[i].completed === true) {
-				// 	console.log('completed', myData[i]); 
-				// 	console.log(completedCheck[i]); 
-					// console.log(completedCheck.checked); 
-				// 	completedCheck[i].checked = true;
-				// }
+
 				taskObject.storeData(this.allTasks, id, false, thisCompleted);
 			} 
 		},
 		checkDateDue: function(){
-			var todayLong = new Date();
 			var today = new Date().toISOString().slice(0,10);
-			// console.log(today);
 
 			var tomorrow = new Date(todayLong.getFullYear(), todayLong.getMonth(), todayLong.getDate() + 1);
 			tomorrow = tomorrow.toISOString().slice(0,10);
@@ -83,10 +83,8 @@ import '../css/global.scss';
 			});
 		},
 		checkOverDue: function(){
-			var todayLong = new Date();
 			var yesterday = new Date(todayLong.getFullYear(), todayLong.getMonth(), todayLong.getDate() - 1);
 			yesterday = yesterday.toISOString().slice(0,10);
-			// console.log(yesterday);
 
 			this.allTasks.filter(function(val, i){
 				if(val.dueDate <= yesterday){
@@ -104,31 +102,42 @@ import '../css/global.scss';
 	var handlerObject = {
 		createTask: function(e) {
 			e.preventDefault();
+			//Get the form values
 			var taskName = document.getElementById('name').value;
 			var taskDescription = document.getElementById('description').value;
 			var taskDueDate = document.getElementById('dueDate').value;
 			var taskCompleted = false;
 			var taskDueNow = taskObject.checkDateDue();
 			var taskOverdue = taskObject.checkOverDue();
+
+			//Get next id from storage
 			if(viewObject.retrievedData()){
 				var taskId = viewObject.retrievedData().length;
 			}else{
 				var taskId = 0;
 			}
 			
+			//Create our new object
 			taskObject.createTask(taskId, taskName, taskDescription, taskDueDate, taskCompleted, taskDueNow, taskOverdue);
 
 			taskObject.getItemById(taskId, taskObject.allTasks); //get id
 			
+			//Updates from our functions
 			taskObject.checkDateDue();
 			taskObject.checkOverDue();
 			taskObject.storeData();
 			viewObject.viewTasks();
 
+			//Reset the form
+			document.getElementById('name').value = "";
+			document.getElementById('description').value = "";
 		},
 		removeTask: function(val){
 			taskObject.removeTask(val);
 			viewObject.viewTasks();
+		},
+		viewDetails: function(val){
+			viewObject.viewDetails(val);
 		},
 		toggleComplete: function(task){
 			taskObject.toggleComplete(task);
@@ -209,6 +218,13 @@ import '../css/global.scss';
 				v.classList.remove('hidden');
 			});
 		},
+		viewDetails: function(val){
+			if(val.classList.contains('task-name')){
+				val.nextSibling.classList.toggle('hidden');
+			} else if(val.id === 'view-more'){
+				val.parentNode.nextSibling.classList.toggle('hidden');
+			}
+		},
 		viewTasks: function(){
 			taskList.innerHTML = "";
 			
@@ -219,59 +235,90 @@ import '../css/global.scss';
  
 				myData.forEach(function(v, i) {
 					var taskId = (i);
-					// console.log(taskId);
-					
-					taskList.innerHTML += '<li class="" id="'+taskId+'"><span>'+taskId+'</span><strong><span>'+myData[i].name+'</span></strong><p><span>'+myData[i].description+'</span><span class="date">'+myData[i].dueDate+'</span><span class="complete"><input type="checkbox" id="completedCheck" class="form-check-input"/><label class="form-check-label" for="completed">Completed?</label></span><button id="remove" class="remove-task btn btn-danger" type="button" aria-label="Close">&times;</button></p></li>';
-					
-					
-					//add visual indicators
+					taskList.innerHTML += '<li class="" id="'+taskId+'"><p class="task-name">'+myData[i].name+'<i aria-label="View Details" id="view-more" class="fas fa-chevron-circle-down"></i><span class="complete"><input type="checkbox" id="completedCheck" class="form-check-input"/><label class="form-check-label" for="completed">Completed?</label></span><span class="date"><strong>Due:</strong> '+myData[i].dueDate+'</span><button id="remove" class="remove-task btn btn-danger" type="button" aria-label="Delete Task"><span aria-hidden="true">&times;</span></button></p><p class="hidden"><span>'+myData[i].description+'</span></p><i aria-label="Overdue" style="display:none" class="fas fa-exclamation-triangle overdue-icon"></i><i aria-label="Due soon" style="display:none" class="far fa-bell due-icon"></i></li>';
+
+					//Add Class helpers
 					var listItem = document.getElementById(taskId);
 
 					if(myData[i].dueNow == true) {
-						// console.log(listItem.classList);
 						listItem.classList.add("due-now"); 
 					}
-					
+						
 					if(myData[i].overdue == true) {
 						listItem.classList.add("overdue");
 					}
-						
 
 				}); //for loop
 			} //if...
+
+			// Visually Indicate States
+			var dueItems = document.getElementsByClassName('due-now');
+			var overdueItems = document.getElementsByClassName('overdue');
+			
+			for(var i=0; i<dueItems.length; i++){
+				var due=[];
+				due.push(dueItems[i]);
+				due.forEach(function(v,i){
+					v.getElementsByClassName('due-icon')[i].style="display:block";
+				});
+			}
+			for(var i=0; i<overdueItems.length; i++){
+				var overdueArr=[];
+				overdueArr.push(overdueItems[i]);
+				overdueArr.forEach(function(v,i){
+					console.log(v.getElementsByClassName('overdue-icon')[i]);
+					v.getElementsByClassName('overdue-icon')[i].style="display:block";
+				});
+			}
+			
 		} //viewTasks
 
 	}; //viewObject
 
-// form.reset();
 
 
 	/**
     * Call Events
     */
-	viewObject.viewTasks(); //show tasks
+
+    //show tasks
+	viewObject.viewTasks(); 
 			
-	//Add task
+	//Add
 	document.getElementById("add-task").addEventListener("click", handlerObject.createTask, false);
 
-	//Remove task
+	//Remove
 	document.addEventListener('click', function(event){
-		// console.log(event);
-		if(event.target && event.target.id == 'remove'){
-			// console.log(event.target.parentNode.parentNode);
+		if(event.target && event.target.parentNode.id === 'remove'){
 			handlerObject.removeTask(event.target.parentNode.parentNode);
 		}
 	});
 
-	//Complete task
+	document.addEventListener('keypress', function(event){
+		console.log('key press 13: ', event.target);
+		if(event.which === 13 && event.target.id === 'remove') {
+			console.log('key press 13', event.target.id);
+			handlerObject.removeTask(event.target.parentNode.parentNode);
+		}
+	});
+
+
+	//Complete
 	document.addEventListener('click', function(event){
 		if(event.target && event.target.id === 'completedCheck') {
 			handlerObject.toggleComplete(event.target.parentNode.parentNode.parentNode);
 		}
 	}, false);
 
+	document.addEventListener('keypress', function(event){
+		if(event.which === 13 && event.target.id === 'completedCheck') {
+			handlerObject.toggleComplete(event.target.parentNode.parentNode.parentNode);
+		}
+	}, false);
 
-	//Filter tasks
+	
+
+	//Filters
 	document.addEventListener('click', function(event){
 		if(event.target && event.target.id === 'filter-duesoon') {
 			handlerObject.filterDue(event.target);
@@ -296,6 +343,12 @@ import '../css/global.scss';
 		}
 	});
 
+	document.addEventListener('click', function(event){
+		if(event.target && event.target.className === "task-name" || event.target && event.target.id === "view-more"){
+			handlerObject.viewDetails(event.target);
+		}
+	}, false);
+	
 
 })();
 
